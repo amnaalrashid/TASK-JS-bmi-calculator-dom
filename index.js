deb7e64abae0c2107486758a822fd1702088c83c
amnaalrashid <amna.alrashid@gmail.com>
2024-08-05 20:27:02 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,62 @@
-function calculateBMI() {
-  // Don't edit the code
-  let age = document.getElementById("age").value;
-  let weight = document.getElementById("weight").value;
-  let height = document.getElementById("height").value;
-  let male = document.getElementById("m").checked;
-  let female = document.getElementById("f").checked;
-  console.table({ age, weight, height, male, female });
+/**
+ * Part 1: Creating Variables and Constants
+ * In this file you should define the following variables with the exact names
+ *
+ * 1. fullName      -> assign it a value of your full name
+ * 2. yearOfBirth   -> assign it a value of your year of birth
+ * 3. hobby         -> assign it a value of your favorite hobby
+ * 4. funFact       -> assign it a value of some fun fact about yourself
+ * 5. image         -> assign it a value of a url of your image or ant image that represents you online
+ */
 
-  // You are going to write the code here before the curly braces 👇🏻
-  height = height / 100;
-  let bmi = weight / (height * height);
+// Part 1 answer 👇🏻 ...
 
-  let bmiCategory;
-  if (bmi < 18.5) {
-    bmiCategory = "Underweight";
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    bmiCategory = "Healthy Weight";
-  } else if (bmi >= 25.0 && bmi <= 29.9) {
-    bmiCategory = "Overweight";
-  } else {
-    bmiCategory = "Obesity";
-  }
+let fullName = "Amnah";
+let yearOfBirth = "1995";
+let hobby = "Reading";
+let funFact = "I enjoy cooking!";
+const image =
+  "https://us.123rf.com/450wm/watcartoon/watcartoon2204/watcartoon220400021/186469783-children-boy-thinking-idea-and-books-cute-kid-image-in-classroom-in-circle-icon.jpg?ver=6";
+console.log(fullName);
+console.log(yearOfBirth);
+console.log(hobby);
+console.log(funFact);
+console.log(image);
 
-  alert(bmiCategory);
+/**
+ * Part 2: String Interpolation
+ * Create the following new variables that interpolate
+ * the variables defined above into strings.
+ *
+ * 1. fullNameString      -> assign it to: My name is {fullName}
+ * 2. yearOfBirthString   -> assign it to: I am {YOUR_AGE}, and make sure you calculate your age from your year of birth
+ * 3. hobbyString         -> assign it to: My hobby is {YOUR_HOBBY}
+ *
+ * Uncomment the following lines and start interpolating.
+ * You can uncomment a line by removing the // at the start
+ */
+
+const fullNameString = `My name is ${"Amnah Alrashed"}`;
+const yearOfBirthString = `My year of birth ${1995}`;
+const hobbyString = `My hobby ${"Reading"}`;
+
+/**
+ * Part 3: Re-assignment
+ * Increment your hacker score
+ * */
+
+let stars = 0;
+
+function incrementBy1() {
+  // Increment stars by 1 👇🏻
+}
+function decrementBy1() {
+  // decrement stars by 1 👇🏻
+}
+
+function incrementBy2() {
+  // Increment stars by 2 👇🏻
+}
+function decrementBy2() {
+  // decrement stars by 2 👇🏻
 }
