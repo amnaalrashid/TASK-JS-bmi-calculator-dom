dc7b460310cdc605cfce080f412fd5dc7c37ac39
amnaalrashid <amna.alrashid@gmail.com>
2024-08-05 14:59:22 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -8,4 +8,19 @@ function calculateBMI() {
   console.table({ age, weight, height, male, female });
 
   // You are going to write the code here before the curly braces 👇🏻
+  height = height / 100;
+  let bmi = weight / (height * height);
+
+  let bmiCategory;
+  if (bmi < 18.5) {
+    bmiCategory = "Underweight";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    bmiCategory = "Healthy Weight";
+  } else if (bmi >= 25.0 && bmi <= 29.9) {
+    bmiCategory = "Overweight";
+  } else {
+    bmiCategory = "Obesity";
+  }
+
+  alert(bmiCategory);
 }
